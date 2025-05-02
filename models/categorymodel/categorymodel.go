8a34806e2a9b8be487dec3d7bb4564240e2e9d45
AQodS <qoddri@gmail.com>
2025-05-02 16:03:32 +0700

    feat: edit category

--- models/categorymodel/categorymodel.go
+++ models/categorymodel/categorymodel.go
@@ -17,7 +17,7 @@ func GetAll() []entities.Category {
 
 	for rows.Next() {
 		var category entities.Category
-		err := rows.Scan(&category.Id, &category.Name, &category.CreatedAt, &category.UpdatedAt)	
+		err := rows.Scan(&category.Id, &category.Name, &category.CreatedAt, &category.UpdatedAt)
 		if err != nil {
 			panic(err)
 		}
@@ -31,7 +31,7 @@ func GetAll() []entities.Category {
 func Create(category entities.Category) bool {
 	result, err := config.DB.Exec(
 		`INSERT INTO categories (name, created_at, updated_at)
-		VALUES (?, ?, ?)`, 
+		VALUES (?, ?, ?)`,
 		category.Name, category.CreatedAt, category.UpdatedAt,
 	)
 	if err != nil {
@@ -42,4 +42,36 @@ func Create(category entities.Category) bool {
 		panic(err)
 	}
 	return lastInsertId > 0
+}
+
+func Detail(id int) entities.Category {
+	row := config.DB.QueryRow(
+		`SELECT id, name FROM categories WHERE id = ?`,
+		id,
+	)
+
+	var category entities.Category
+	err := row.Scan(&category.Id, &category.Name)
+	if err != nil {
+		panic(err)
+	}
+
+	return category
+}
+
+func Update(id int, category entities.Category) bool {
+	query, err := config.DB.Exec(
+		`UPDATE categories SET name = ?, updated_at = ? WHERE id = ?`,
+		category.Name, category.UpdatedAt, id,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	result, err := query.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+
+	return result > 0
 }
