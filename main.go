145ebd41b03574f44ff9fb2b4e1637a9358ed115
AQodS <qoddri@gmail.com>
2025-05-02 19:30:02 +0700

    feat: create product

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"goweb/config"
 	"goweb/controllers/categorycontroller"
 	"goweb/controllers/homecontroller"
+	"goweb/controllers/productcontroller"
 	"log"
 	"net/http"
 )
@@ -19,6 +20,13 @@ func main() {
 	http.HandleFunc("/categories/add", categorycontroller.Add)
 	http.HandleFunc("/categories/edit", categorycontroller.Edit)
 	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	// products
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/add", productcontroller.Add)
+	http.HandleFunc("/products/detail", productcontroller.Detail)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
 
 	log.Println("Server is running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
